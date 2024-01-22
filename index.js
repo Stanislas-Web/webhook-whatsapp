dc7f9dcf5b72468aaeb898ff3dbf560fd389c4f8
Stanislas-Web <stamislasmakengo1@gmail.com>
2024-01-22 15:32:50 +0100

    update v app

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.post("/webhook",(req,res)=>{ //i want some
                        messaging_product:"whatsapp",
                        to:from,
                        text:{
-                           body:"Hi.. I'm Prasath, your message is "+msg_body
+                           body:"Bonjour je suis Stanislas Makengo , ton message est "+msg_body
                        }
                    },
                    headers:{
